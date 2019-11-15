3299db5fab582ae02bffd24e7851a0dffa90f9b7
dlwagner <wagner.don@gmail.com>
2019-11-15 14:39:31 -0500

    add new user button with input fields

--- client/users-vue-app/src/components/UsersList.vue
+++ client/users-vue-app/src/components/UsersList.vue
@@ -3,22 +3,35 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">List of users</div>
+                <div class="panel-heading">List of Users</div>
                 <div class="panel-body">
                     <table class="table">
                         <thead>
                           <tr>
+                            <th>Id</th>
                             <th>Name</th>
                             <th>Email</th>
                           </tr>
                         </thead>
                         <tbody>
                           <tr v-for="user in users" v-bind:key="user.id">
+                            <td>{{user.id}}</td>
                             <td>{{user.name}}</td>
                             <td>{{user.email}}</td>
                           </tr>
                       </tbody>
                     </table>
+                    <div class="row">
+                        <button class="btn btn-success" v-on:click="addUser()">Add</button>
+                        <fieldset class="form-group">
+                          <label>Name</label>
+                          <input type="text" class="form-control" v-model="name">
+                        </fieldset>
+                        <fieldset class="form-group">
+                          <label>Email</label>
+                          <input type="text" class="form-control" v-model="email">
+                        </fieldset>
+                    </div>
                 </div>
             </div>
         </div>
@@ -36,13 +49,39 @@ export default {
     }
   },
   /* eslint-disable no-console */
+  methods: {
+    retrieveAllUsers () {
+      axios.get('http://localhost:8080/demo/all')
+      .then(response => (this.users = response.data))
+    },
+    addUser() {
+      axios.post('http://localhost:8080/demo/add')
+    }
+  },
+  created() {
+        this.retrieveAllUsers();
+    }
+  /* eslint-enable no-console */
+}
+</script>
+
+<!--<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  /* eslint-disable no-console */
   mounted () {
     axios.get('http://localhost:8080/demo/all')
       .then(response => (this.users = response.data))
   } 
   /* eslint-enable no-console */
 }
-</script>
+</script>-->
 
 <style>
 @import url(https://unpkg.com/bootstrap@4.1.0/dist/css/bootstrap.min.css);
